5c8a458f08b9cdbec5b44fb9d922e26c1ed59046
Yili Zhou (Wicresoft Co <v-yilzho@microsoft.com>
2019-05-24 16:15:53 +0800

    third attempt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Node.js TEST!");
+    response.end("Third attempt!");
 });
 
 const port = process.env.PORT || 1337;
